74dc1015d6145bb67e2b53deb0fe63bc2133ce5d
narhz <narhz@pm.me>
2019-09-20 13:32:12 -0500

    add constants.py

---
+++ constants.py
@@ -0,0 +1,18 @@
+from os import getcwd
+
+
+
+
+class Urls:
+    self.OLD_REDDIT = 'https://old.reddit.com'
+    self.PROXIES = 'https://api.proxyscrape.com/?request=getproxies&proxytype=http&timeout=10000&country=all&ssl=all&anonymity=all'
+    self.IP_CHECK = 'https://api.ipify.org'
+    self.REGESTER = 'https://www.reddit.com/register/'
+
+
+class Paths:
+    self.DRIVER_PATH = os.getcwd() + '/geckodriver'
+
+
+class Elements:
+    pass
