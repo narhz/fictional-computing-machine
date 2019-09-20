8e6cde7b49cb561773d0ee77fc6dd7dd2a91f6ab
narhz <narhz@pm.me>
2019-09-20 11:53:37 -0500

    add function to get/update proxy list

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.proxy import Proxy, ProxyType
 
+import requests
+
 import os
 
 from time import sleep
+from random import randint
 
+
+
+
+def get_proxies():
+    req = requests.get('https://api.proxyscrape.com/?request=getproxies&proxytype=http&timeout=10000&country=all&ssl=all&anonymity=all').content
+    with open('proxy_list.txt', 'wb') as file:
+        file.write(req)
 
 
 class Driver:
@@ -13,41 +23,31 @@ class Driver:
         options = Options()
         options.headless = False
 
-        path = os.getcwd() + '/geckodriver'
+        self.driver_path = os.getcwd() + '/geckodriver'
 
-        with open('proxy_list.txt') as file:
-            proxy_list = file.readlines()
+        # with open('proxy_list.txt') as file:
+        #     proxy_list = file.readlines()
+        #
+        # for proxy in proxy_list:
+        #     PROXY = proxy.replace('\n', '')
+        #     webdriver.DesiredCapabilities.FIREFOX['proxy']={
+        #         "httpProxy":PROXY,
+        #         "ftpProxy":PROXY,
+        #         "sslProxy":PROXY,
+        #         "proxyType":"MANUAL",
+        #     }
+        #     try:
+        #         self.driver = webdriver.Firefox(executable_path=path,options=options)
+        #         self.driver.set_page_load_timeout(10)
+        #     except:
+        #         self.driver.close()
 
-        for proxy in proxy_list:
-            PROXY = proxy.replace('\n', '')
-            webdriver.DesiredCapabilities.FIREFOX['proxy']={
-                "httpProxy":PROXY,
-                "ftpProxy":PROXY,
-                "sslProxy":PROXY,
-                # "noProxy":None,
-                "proxyType":"MANUAL",
-                # "autodetect":False
-            }
-            try:
-                driver = webdriver.Firefox(executable_path=path, options=options)
-                driver.set_page_load_timeout(10)
-                driver.get('https://api.ipify.org')
-                print(driver.page_source)
-                driver.close()
-            except:
-                driver.close()
-
-
-    def test(self):
-        self.driver.get('https://api.ipify.org')
-        sleep(5)
-        self.driver.close()
 
 
 
 
 if __name__ == '__main__':
-    Driver()
+    get_proxies()
 
 
 
